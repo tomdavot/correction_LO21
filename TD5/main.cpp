7fb6fed9c5975ab91202aa0dbc7440ea7e4c7e45
Tom Davot <tom.davot@lirmm.fr>
2022-10-10 12:06:38 +0200

    correction TD5

---
+++ TD5/main.cpp
@@ -0,0 +1,34 @@
+#include "pioche.h"
+#include "plateau.h"
+using namespace Set;
+
+int main() {
+  try {
+
+    printCouleurs();
+    printNombres();
+    printFormes();
+    printRemplissages();
+ 
+    Jeu &jeu= Jeu::getJeu();
+    //    std:: cout << jeu.getCarte(0) << std::endl;
+    //    std:: cout << jeu.getCarte(83) << std::endl;
+    Plateau p;
+    for(auto it : p){
+	
+    }
+    Pioche p1, p2;
+    for(size_t i=0; i<5; ++i){
+      std::cout << "Jeu 1: " << p1.piocher() << std::endl;
+      std::cout << "Jeu 2: " << p2.piocher() << std::endl;
+    }
+
+  }
+  catch (SetException& e) {
+    std::cout << e.getInfo() << "\n";
+  }
+	
+
+
+  return 0;
+}
